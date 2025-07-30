fbe1926cb246a59a75d4317ccd475a9e56c825a2
Eric Freiberg <ericfreiberg@outlook.de>
2025-07-30 12:05:32 +0200

    Fix: Avoid runtime error in userInput_double
    userInput_double accidentally used int instead of double, which is false. To avoid runtime errors, this was fixed.

--- userInput.c
+++ userInput.c
@@ -144,7 +144,7 @@ int userInput_double (double *buffer, char* prompt) {
         return 1; // Error in input
     }
 
-    int value = strtod(input, &endptr);
+    double value = strtod(input, &endptr);
 
     if (errno == ERANGE || (endptr == input) || (*endptr != '\0')) {
         fprintf(stderr, "User-provided input not a valid double.\n");
