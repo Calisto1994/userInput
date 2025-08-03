ab537ac4fcb7599696434e2baca9d86736290c6c
Eric Freiberg <ericfreiberg@outlook.de>
2025-08-03 19:09:31 +0200

    Prepare for v2.0 Some files weren't successfully transferred in the last commit; Adding them now.

---
+++ ERRORS.h
@@ -0,0 +1,14 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#define LANG_ERRMSG_GENERAL     1
+#define LANG_ERRMSG_YN          2
+#define LANG_ERRMSG_INT         3
+#define LANG_ERRMSG_INT_RANGE   4
+#define LANG_ERRMSG_DOUBLE      5
+#define LANG_ERRMSG_MEMORY      6
+
+#define LANG_YESCHAR            'j'
+#define LANG_NOCHAR             'n'
+
+#endif
---
+++ userInput_errors.h
@@ -0,0 +1,11 @@
+#ifndef UINPUT_ERRORS
+#define UINPUT_ERRORS
+
+#define UINPUT_ERRMSG_GENERAL     1
+#define UINPUT_ERRMSG_YN          2
+#define UINPUT_ERRMSG_INT         3
+#define UINPUT_ERRMSG_INT_RANGE   4
+#define UINPUT_ERRMSG_DOUBLE      5
+#define UINPUT_ERRMSG_MEMORY      6
+
+#endif
